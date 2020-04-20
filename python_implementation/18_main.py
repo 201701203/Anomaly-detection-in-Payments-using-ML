a6f90788770c59c376c2bde3261a56d57a494274
Jaymin <201701203@daiict.ac.in>
2020-04-20 15:46:24 +0530

    log reg

--- python_implementation/18_main.py
+++ python_implementation/18_main.py
@@ -7,23 +7,25 @@ from mpl_toolkits.mplot3d import axes3d
 
 # import model
 import Isolation_Forest as iso
-import Local_Outlier_Factor
-import Logistic_Regression
-import Support_Vector_Machine
+import Local_Outlier_Factor as lof
+import Logistic_Regression as log_reg
+import Support_Vector_Machine as svm
 
 #read data from excel (Data-set)
-data=pd.read_excel('creditcard.xlsx')   # https://www.kaggle.com/mlg-ulb/creditcardfraud
-x=np.array([data.iloc[1:, 1:29]]).T     
-y=np.array([data.iloc[1:,30]]).T        # target class
+data=pd.read_csv(r"C:\\Users\\hp\\Desktop\\ML\\creditcard.csv") 
+x=data.iloc[1:, 1:29]     
+y=data.iloc[1:,30]       # target class
 
 #for making it normal
 x = x-np.mean(x)
 x = x/np.std(x)
 
+#--------Plot the data-----------------
+
 
 #--------------------------------1---Isolation Forest----------------------------------#
 F = iso.iForest(X=x,ntrees=500, sample_size=256)    #Create Forest
-S=F.compute_paths(X_in=X)
+S=F.compute_paths(X_in=x)
 
 
 #--------------------------------2---Local outlier Factor------------------------------#
@@ -31,8 +33,10 @@ S=F.compute_paths(X_in=X)
 
 
 #--------------------------------3---Logistic Regression-------------------------------#
-
+model = log_reg.LogisticRegression(lr=0.1, num_iter=120000) # Learning rate = 0.1 and Number of Iteration 120000
+model.fit(x, y)
+preds = model.predict(X)
+model.theta
 
 
 #--------------------------------4---Support vector machine----------------------------#
-
